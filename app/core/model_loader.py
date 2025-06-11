7e8dca7b6687fc3999de66c050d0daa01527178c
DhafinAghniya <148194082+DhafinAghniya@users.noreply.github.com>
2025-06-12 01:40:15 +0700

    Update model loader

--- app/core/model_loader.py
+++ app/core/model_loader.py
@@ -1,11 +1,14 @@
+import os
+import gdown
 from tensorflow.keras.models import load_model
 
-_model = None
+MODEL_PATH = 'model/model.h5'
+GDRIVE_URL = 'https://drive.google.com/uc?id=1OOFw4ddZvo7rppsWL-AamAkUcSrc5zx_'
 
 def get_model():
-    global _model
-    if _model is None:
-        print(">> Loading model.h5 ...")
-        _model = load_model("model/model.h5")
-        print(">> Model loaded successfully.")
-    return _model
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+        print(">> Downloading model from Google Drive...")
+        gdown.download(GDRIVE_URL, MODEL_PATH, quiet=False)
+    print(">> Loading model.h5 ...")
+    return load_model(MODEL_PATH)
